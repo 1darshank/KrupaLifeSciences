f8acd68851306f9ca323d508840f9de509fd6ebd
1darshank <50342146+1darshank@users.noreply.github.com>
2019-05-06 22:07:24 -0400

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,297 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Krupa Life Sciences | CONTACT US</title>
+    <!-- favicon -->
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <!-- fontawesome -->
+    <link rel="stylesheet" href="assets/css/fontawesome-all.min.css">
+    <!-- flaticon -->
+    <link rel="stylesheet" href="assets/css/flaticon.css">
+    <!-- slicknav -->
+    <link rel="stylesheet" href="assets/css/slicknav.min.css">
+    <!-- magnific popup css -->
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+    <!-- owl carousel css -->
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <!-- stylesheet -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <!-- responsive -->
+    <link rel="stylesheet" href="assets/css/responsive.css">
+    
+
+</head>
+<body>
+<!-- support bar area start -->
+    <div class="support-bar">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 col-sm-3 col-xs-12">
+                    <a href="index.html" class="logo">
+                        <img src="assets/img/logo.png" alt="logo">
+                    </a>  
+                </div>
+                <div class="col-md-7 col-sm-9 col-xs-12">
+                    <div class="support-right">
+                        <div class="single-support-box">
+                            <div class="icon">
+                                <i class="fas fa-phone"></i>
+                            </div>
+                            <div class="content">
+                                <a href="tel:+8800112375">+732 572 2531</a>
+                                <!-- <a href="tel:+8800112375">+848 123 ####</a> -->
+                            </div>
+                        </div>
+                        <div class="single-support-box">
+                            <div class="icon">
+                                <i class="fas fa-map-marker-alt"></i>
+                            </div>
+                            <div class="content">
+                                <a href="mailto:info@netmax.com">info@KrupaLifeSciences.com</a>
+                                <!-- <a href="mailto:netmax@ymailx.com">contact@KrupaLifeSciences.com</a> -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-2 col-sm-2 col-xs-12">
+                   <div class="support-btn">
+                        <a href="./contact.php" class="boxed-btn">get a quote</a>
+                   </div>
+                </div>
+            </div>
+        </div>
+    </div> <!--end support bar area -->
+
+<!-- header-area start -->
+    <header class="header-area header-area-bg">
+<!-- navbar area start -->
+<nav class="navbar-area">
+    <div class="container">
+        <div class="row navbar-bg">
+            <div class="col-md-9 col-sm-6 col-xs-3">
+                <div class="responsive-menu"></div>
+                <ul id="primary-menu">
+                    <li>
+                        <a href="./index.html">Home</a>
+                    </li>
+                    <li>
+                        <a href="./about.html">About Us</a>
+                    </li>
+                    <li>
+                        <a href="./products.html">Products</a>
+                    </li>
+                    <li>
+                        <a href="./contact.php">Contact</a>
+                    </li>
+                   
+                </ul>
+            </div>
+            <div class="col-md-3 col-sm-6 col-xs-9">
+                <div class="navbar-right-area">
+                    <div class="support-btn hidden-desktop">
+                        <a href="./contact.php" class="boxed-btn">get a quote</a>
+                    </div>
+                   
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
+<!--end nav bar-->
+        <div class="container" id="home">
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1 text-center">
+                    <div class="header-inner-content"><br />
+                        <h1>Contact Us</h1>
+                       
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </header><!-- end header-area -->
+    
+<!-- features area start -->
+<section class="features-area" id="netmax_business">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="left-content">
+                    <h2><span>For Inquiries and Feedback</span> Reach Us</h2>
+                    Krupa Life Sciences <br/>
+                    7 Deer park Drive,<br/>
+                    Monmouth Junction,<br />
+                    New Jersey <br/>
+                    08852 <br/>
+                    <div class="row">
+                        <div class="col-md-6 col-sm-6 col-xs-6">
+                            <ul>
+                                <li>
+                                    <a href="mailto:info@KrupaLifeSciences.com?Subject=Hello%20LifeSciences" target="_top">- info@KrupaLifeSciences.com</a>
+                                </li>
+                                <li>
+                                    <a href="mailto:contact@KrupaLifeSciences.com?Subject=Hello%20LifeSciences" target="_top">- contact@KrupaLifeSciences.com</a>
+                                </li>
+                                <li>
+                                    - Ph: +732 572 2531
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-md-6 col-sm-6 col-xs-6">
+                            <ul>
+                                <li>
+                                    - Add us on Facebook
+                                </li>
+                                <li>
+                                    - Follow us on Twitter 
+                                </li>
+                                <li>
+                                    - Follow us on Instagram 
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                     <br />
+
+                <!-- CONTACT FORM HERE -->
+
+              
+<h2>Contact Form</h2>
+<form method="post" action="mail.php">
+
+  <div class="form-group">
+    <input type="text" class="form-control" id="InputName" name="name" placeholder="Name">
+  </div>
+
+  <div class="form-group">
+    <input type="email" class="form-control" id="InputEmail1" name="email" placeholder="Email">
+  </div>
+
+  <div class="form-group">
+    <input type="text" class="form-control" id="InputPhone" name="phone" placeholder="Phone">
+  </div>
+
+  <div class="form-group">
+    <input type="text" class="form-control" id="InputCompanye" name="company" placeholder="Company">
+  </div>
+
+  <div class="form-group">
+    <textarea class="form-control" rows="5" id="comment" name="message" placeholder="Your Message"></textarea>
+  </div>
+
+  <button type="submit" class="btn btn-primary btn-lg btn-block">Send Message</button>
+</form>
+
+
+
+
+
+                <!--  Contact form End Here -->
+
+                </div>
+            </div>
+
+            <div class="col-md-6">
+                <div class="right-content">
+                    <img src="assets/img/contact-page-title.jpg" alt="features">
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+
+
+<!-- features area end -->
+
+
+
+
+
+
+<!-- footer area start -->
+<footer class="footer-area">
+    <div class="footer-inner-wrapper">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   
+                    <div class="footer-bottom">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="widget about">
+                                    <div class="widget-body">
+                                        <a href="index.html" class="footer-logo">
+                                            <img src="assets/img/footer-logo.png" alt="footer logo">
+                                        </a>
+                                        <p>At KLS, we produce are a versatile range of enzymes like amylases, proteases, cellulases, xylanase, beta glucanase, glucoamylase and Catalase; using the most sophisticated fermentation technologies. </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="widget contact">
+                                    <div class="widget-title">
+                                        <h4>Get In Touch</h4>
+                                    </div>
+                                    <div class="widget-body">
+                                        <ul>
+                                            <li>
+                                                <i class="fas fa-home"></i> 7 Deer park Drive, <br /> 
+                                                Monmouth Junction, <br /> NJ 08852</li>
+                                           
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="widget subscribe">
+                                    <div class="widget-title">
+                                        <h4>Testimonial</h4>
+                                    </div>
+                                    <div class="widget-body">
+                                       <p>Customer review comes here</p>
+                                    </div>
+                                   
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="copyright-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                        Copyright &copy; KrupaLifeSciences 2018. All right reserved.
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- footer area end -->
+
+
+
+    <!-- jquery -->
+    <script src="assets/js/jquery.js"></script>
+    <!-- bootstrap -->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <!-- slicknav -->
+    <script src="assets/js/jquery.slicknav.min.js"></script>
+    <!-- magnifiq popup  -->
+    <script src="assets/js/jquery.magnific-popup.min.js"></script>
+    <!-- owl carousel -->
+    <script src="assets/js/owl.carousel.min.js"></script>
+   
+    <!-- main -->
+    <script src="assets/js/main.js"></script>
+</body>
+</html>
